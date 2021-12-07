146200c1f04848a65875ddc247439bcc2c4e94f8
Данил Бадай <paladen4@gmail.com>
2021-12-08 01:37:37 +0300

    New tests

--- NUnitFileParserTest/UnitTest1.cs
+++ NUnitFileParserTest/UnitTest1.cs
@@ -1,22 +1,27 @@
 using NUnit.Framework;
-using FileParser;
+using Parser;
 
 namespace NUnitFileParserTest
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+
+        [Test]
+        [Category("StaticParser")]
+        [TestCase(2, "Wo", new string[] { "Text", "SomethingWoWoWoWWoooWo", "Happend" })]
+        public void Test_ToFindSubStringCheck(int lineNum, string subString, string[] lines)
         {
-            
+            int expectedResult = 5;
+            Assert.AreEqual(expectedResult, StaticParser.FindSubString(lineNum, subString, lines));
         }
 
         [Test]
         [Category("StaticParser")]
-        [TestCase(2, "Wo", new string[] {"Text", "SomethingWoWoWoWWoooWo", "Happend" })]
-        public void Test_ToFindSubStringCheck(int lineNum, string subString, string[] lines)
+        [TestCase(1, "_", "45", new string[] { "123_6", "Hello" })]
+        public void Test_ToFindAndReplaceSubStringCheck(int lineNum, string subStrToSearch, string subStrToReplace, string[] lines)
         {
-            Assert.AreEqual(5, StaticParser.FindSubString(lineNum, subString, lines));
+            string expectedResult = "123456";
+            Assert.AreEqual(expectedResult, StaticParser.FindAndReplaceSubString(lineNum, subStrToSearch, subStrToReplace , lines));
         }
     }
 }
--- SoftServeTask1UnitTest/UnitTest1.cs
+++ SoftServeTask1UnitTest/UnitTest1.cs
@@ -7,13 +7,11 @@ namespace SoftServeTask1UnitTest
     public class Tests
     {
         TriangleController controller;
-        View view;
 
         [SetUp]
         public void Setup()
         {
             controller = new TriangleController();
-            view = new View();
         }
 
         [Test]
@@ -21,8 +19,6 @@ namespace SoftServeTask1UnitTest
         [TestCase(10.0, 10.0, 15.0)]
         public void Test_IsTrianglePositive(double a, double b, double c)
         {
-            Setup();
-            //bool expectedResult = true;
             bool actualResult = controller.IsTriangle(a, b, c);
             Assert.IsTrue(actualResult, "I should not be false");
         }
@@ -32,7 +28,6 @@ namespace SoftServeTask1UnitTest
         [TestCase(100.0, 10.7, 15.24)]
         public void Test_IsTriangleNegative(double a, double b, double c)
         {
-            Setup();
             bool result = controller.IsTriangle(a, b, c);
             Assert.IsFalse(result, "I should not be true");
         }
@@ -41,7 +36,6 @@ namespace SoftServeTask1UnitTest
         public void Clear()
         {
             controller = null;
-            view = null;
         }
     }
 }
